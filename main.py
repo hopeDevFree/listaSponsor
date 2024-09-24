8a7dcb07ed2e77e50d1265a02c19de22260dbf97
hopeDev <151857257+hopeDevFree@users.noreply.github.com>
2024-09-24 12:25:54 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import re
+from contextlib import closing
 
 import psycopg2
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
@@ -6,7 +7,6 @@ from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 import tgcrypto
-from psycopg2 import pool
 from dotenv import load_dotenv
 import os
 
@@ -17,187 +17,202 @@ load_dotenv()
 app = Client(name="SponsorLVB", api_id=os.environ["api_id"], api_hash=os.environ["api_hash"],
              bot_token=os.environ["bot_token"])
 
-connection_pool = psycopg2.pool.SimpleConnectionPool(1, 20,
-                                                     database=os.environ["db_name"],
-                                                     host=os.environ["db_host"],
-                                                     user=os.environ["db_user"],
-                                                     password=os.environ["db_password"],
-                                                     port=os.environ["db_port"]
-                                                     )
-
 
 @app.on_message(filters.command('start') & filters.private)
 async def start_command(app, message):
-    conn = connection_pool.getconn()
+    with closing(psycopg2.connect(
+            database=os.environ["db_name"],
+            host=os.environ["db_host"],
+            user=os.environ["db_user"],
+            password=os.environ["db_password"],
+            port=os.environ["db_port"]
+    )) as conn:
+        with conn.cursor() as cur:
 
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM users WHERE id=%s", (message.from_user.id,))
-    if cur.fetchone() is None:
-        cur.execute("INSERT INTO users(id, date) VALUES (%s, %s)", (message.chat.id, message.date))
+            cur.execute("SELECT * FROM users WHERE id=%s", (message.from_user.id,))
+            if cur.fetchone() is None:
+                cur.execute("INSERT INTO users(id, date) VALUES (%s, %s)", (message.chat.id, message.date))
 
-    base_buttons_list = [[InlineKeyboardButton('Registra canali ✏️', callback_data="channelSU/0")]]
+            base_buttons_list = [[InlineKeyboardButton('Registra canali ✏️', callback_data="channelSU/0")]]
 
-    if message.from_user.id == 793550967 or message.from_user.id == 5453376840:
-        welcome_text = """👷🏻 <i>Ciao admin!     
-
+            if message.from_user.id == 793550967 or message.from_user.id == 5453376840:
+                welcome_text = """👷🏻 <i>Ciao admin!     
+        
 👇🏻 Digita i pulsanti sotto per effettuare l'operazione che preferisci!</i>"""
 
-        base_buttons_list.append([InlineKeyboardButton("Canali registrati 👁️", callback_data="allChannels"),
-                                  InlineKeyboardButton("Preview lista 📄", callback_data="previewList")])
+                base_buttons_list.append([InlineKeyboardButton("Canali registrati 👁️", callback_data="allChannels"),
+                                          InlineKeyboardButton("Preview lista 📄", callback_data="previewList")])
 
-        await message.reply(
-            text=welcome_text,
-            reply_markup=InlineKeyboardMarkup(base_buttons_list),
-            disable_web_page_preview=True)
+                await message.reply(
+                    text=welcome_text,
+                    reply_markup=InlineKeyboardMarkup(base_buttons_list),
+                    disable_web_page_preview=True)
 
-    conn.commit()
-    connection_pool.putconn(conn)
+            conn.commit()
 
 
 @app.on_callback_query()
 async def callback_query(app, callbackQuery):
-    conn = connection_pool.getconn()
+    with closing(psycopg2.connect(
+            database=os.environ["db_name"],
+            host=os.environ["db_host"],
+            user=os.environ["db_user"],
+            password=os.environ["db_password"],
+            port=os.environ["db_port"]
+    )) as conn:
+        with conn.cursor() as cur:
+            cur.execute("SELECT * FROM users WHERE id=%s", (callbackQuery.from_user.id,))
+            user = cur.fetchone()
 
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM users WHERE id=%s", (callbackQuery.from_user.id,))
-    user = cur.fetchone()
+            cur.execute("SELECT * FROM list")
+            list_obj = cur.fetchone()
 
-    cur.execute("SELECT * FROM list")
-    list_obj = cur.fetchone()
+            if callbackQuery.data == "menu":
 
-    if callbackQuery.data == "menu":
+                base_buttons_list = [[InlineKeyboardButton('Registra canali ✏️', callback_data="channelSU/0")]]
 
-        base_buttons_list = [[InlineKeyboardButton('Registra canali ✏️', callback_data="channelSU/0")]]
-
-        if callbackQuery.from_user.id == 793550967 or callbackQuery.from_user.id == 5453376840:
-            welcome_text = """👷🏻 <i>Ciao admin!     
-
+                if callbackQuery.from_user.id == 793550967 or callbackQuery.from_user.id == 5453376840:
+                    welcome_text = """👷🏻 <i>Ciao admin!     
+        
 👇🏻 Digita i pulsanti sotto per effettuare l'operazione che preferisci!</i>"""
 
-            base_buttons_list.append([InlineKeyboardButton("Canali registrati 👁️", callback_data="allChannels"),
-                                      InlineKeyboardButton("Preview lista 📄", callback_data="previewList")])
+                    base_buttons_list.append([InlineKeyboardButton("Canali registrati 👁️", callback_data="allChannels"),
+                                              InlineKeyboardButton("Preview lista 📄", callback_data="previewList")])
 
-            await callbackQuery.edit_message_text(
-                text=welcome_text,
-                reply_markup=InlineKeyboardMarkup(base_buttons_list),
-                disable_web_page_preview=False)
+                    await callbackQuery.edit_message_text(
+                        text=welcome_text,
+                        reply_markup=InlineKeyboardMarkup(base_buttons_list),
+                        disable_web_page_preview=False)
 
-    if "channelSU" in callbackQuery.data:
+            if "channelSU" in callbackQuery.data:
 
-        signup_pass = callbackQuery.data.split("/")[1]
+                signup_pass = callbackQuery.data.split("/")[1]
 
-        if signup_pass == "0":
-            await callbackQuery.edit_message_text(
-                text="""<i>📄 Invia la lista che verrà inoltrata.</i>""",
+                if signup_pass == "0":
+                    await callbackQuery.edit_message_text(
+                        text="""<i>📄 Invia la lista che verrà inoltrata.</i>""",
 
-                reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('👇🏻 Scrivi', callback_data='none')]]),
-                disable_web_page_preview=False)
+                        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('👇🏻 Scrivi', callback_data='none')]]),
+                        disable_web_page_preview=False)
 
-            cur.execute("UPDATE users SET step = %s, lastbotmessage = %s WHERE id = %s",
-                        ("pass0", callbackQuery.message.id, user[0],))
+                    cur.execute("UPDATE users SET step = %s, lastbotmessage = %s WHERE id = %s",
+                                ("pass0", callbackQuery.message.id, user[0],))
 
-    if callbackQuery.data == "previewList":
-        await app.send_message(chat_id=callbackQuery.from_user.id,
-                               text=f"""<i>👁️ Ecco la lista attuale:""")
+            if callbackQuery.data == "previewList":
+                await app.send_message(chat_id=callbackQuery.from_user.id,
+                                       text=f"""<i>👁️ Ecco la lista attuale:""")
 
-        await app.copy_message(
-            chat_id=callbackQuery.from_user.id,
-            from_chat_id=callbackQuery.from_user.id,
-            message_id=list_obj[0],
-            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('🔙 Home', callback_data='menu')]])
-        )
+                await app.copy_message(
+                    chat_id=callbackQuery.from_user.id,
+                    from_chat_id=callbackQuery.from_user.id,
+                    message_id=list_obj[0],
+                    reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('🔙 Home', callback_data='menu')]])
+                )
 
-    if callbackQuery.data == "allChannels":
-        pattern = r'@\S+'
+            if callbackQuery.data == "allChannels":
+                pattern = r'@\S+'
 
-        match = re.findall(pattern, list_obj[1])
+                match = re.findall(pattern, list_obj[1])
 
-        channels_text = "➕ Questi sono i canali presenti nella lista:\n\n"
-        for channel in match:
-            channels_text = channels_text + f"- {channel}\n"
+                channels_text = "➕ Questi sono i canali presenti nella lista:\n\n"
+                for channel in match:
+                    channels_text = channels_text + f"- {channel}\n"
 
-        await app.send_message(
-            chat_id=callbackQuery.from_user.id,
-            text=channels_text,
-            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('🔙 Home', callback_data='menu')]])
-        )
+                await app.send_message(
+                    chat_id=callbackQuery.from_user.id,
+                    text=channels_text,
+                    reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('🔙 Home', callback_data='menu')]])
+                )
 
-    conn.commit()
-    connection_pool.putconn(conn)
+            conn.commit()
 
 
 # Check passaggi singup
 @app.on_message(filters.private)
 async def signupChannel(app, message):
-    conn = connection_pool.getconn()
+    with closing(psycopg2.connect(
+            database=os.environ["db_name"],
+            host=os.environ["db_host"],
+            user=os.environ["db_user"],
+            password=os.environ["db_password"],
+            port=os.environ["db_port"]
+    )) as conn:
+        with conn.cursor() as cur:
+            cur.execute("SELECT * FROM users WHERE id=%s", (message.from_user.id,))
+            user = cur.fetchone()
 
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM users WHERE id=%s", (message.from_user.id,))
-    user = cur.fetchone()
+            if user[1] == "pass0":
+                list_text = message.text
+                id_message_list = message.id
 
-    if user[1] == "pass0":
-        list_text = message.text
-        id_message_list = message.id
+                cur.execute("SELECT * FROM list")
+                list_obj = cur.fetchone()
 
-        cur.execute("SELECT * FROM list")
-        list_obj = cur.fetchone()
+                if list_obj is None:
+                    cur.execute("INSERT INTO list(id, text) values (%s, %s)", (id_message_list, list_text))
 
-        if list_obj is None:
-            cur.execute("INSERT INTO list(id, text) values (%s, %s)", (id_message_list, list_text))
+                else:
+                    cur.execute("UPDATE list SET id = %s, text = %s WHERE id = %s",
+                                (id_message_list, list_text, list_obj[0]))
 
-        else:
-            cur.execute("UPDATE list SET id = %s, text = %s WHERE id = %s", (id_message_list, list_text, list_obj[0]))
-
-        await message.reply(
-            text="""<i>✅ La lista è stata salvata correttamente! 
-                                    
+                await message.reply(
+                    text="""<i>✅ La lista è stata salvata correttamente! 
+                                            
 Verrà inoltrata il <b>primo</b> giorno di ogni mese alle 18:00.</i>""",
-            reply_markup=InlineKeyboardMarkup(
-                [[InlineKeyboardButton('🔙 Home', callback_data='menu')]]),
-            disable_web_page_preview=False)
+                    reply_markup=InlineKeyboardMarkup(
+                        [[InlineKeyboardButton('🔙 Home', callback_data='menu')]]),
+                    disable_web_page_preview=False)
 
-    conn.commit()
-    connection_pool.putconn(conn)
+            conn.commit()
 
 
 async def send_list_message():
-    conn = connection_pool.getconn()
+    with closing(psycopg2.connect(
+            database=os.environ["db_name"],
+            host=os.environ["db_host"],
+            user=os.environ["db_user"],
+            password=os.environ["db_password"],
+            port=os.environ["db_port"]
+    )) as conn:
+        with conn.cursor() as cur:
+            cur.execute("SELECT * FROM list")
+            list_obj = cur.fetchone()
 
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM list")
-    list_obj = cur.fetchone()
+            pattern = r'@\S+'
 
-    pattern = r'@\S+'
+            match = re.findall(pattern, list_obj[1])
 
-    match = re.findall(pattern, list_obj[1])
+            for channel in match:
+                copied_message = await app.copy_message(
+                    chat_id=channel,
+                    from_chat_id=793550967,
+                    message_id=list_obj[0]
+                )
+                cur.execute("INSERT INTO channels_message(username, id_message) values(%s, %s)",
+                            (channel, copied_message.id))
 
-    for channel in match:
-        copied_message = await app.copy_message(
-            chat_id=channel,
-            from_chat_id=793550967,
-            message_id=list_obj[0]
-        )
-        cur.execute("INSERT INTO channels_message(username, id_message) values(%s, %s)", (channel, copied_message.id))
-
-    conn.commit()
-    connection_pool.putconn(conn)
+            conn.commit()
 
 
 async def delete_list_message():
-    conn = connection_pool.getconn()
+    with closing(psycopg2.connect(
+            database=os.environ["db_name"],
+            host=os.environ["db_host"],
+            user=os.environ["db_user"],
+            password=os.environ["db_password"],
+            port=os.environ["db_port"]
+    )) as conn:
+        with conn.cursor() as cur:
+            cur.execute("SELECT * FROM channels_message")
+            list_objs = cur.fetchall()
 
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM channels_message")
-    list_objs = cur.fetchall()
+            for channel in list_objs:
+                await app.delete_messages(
+                    chat_id=channel[0],
+                    message_ids=channel[1])
+                cur.execute("DELETE FROM channels_message WHERE username=%s", (channel[0],))
 
-    for channel in list_objs:
-        await app.delete_messages(
-            chat_id=channel[0],
-            message_ids=channel[1])
-        cur.execute("DELETE FROM channels_message WHERE username=%s", (channel[0],))
-
-    conn.commit()
-    connection_pool.putconn(conn)
+            conn.commit()
 
 
 scheduler = AsyncIOScheduler(timezone="Europe/Rome")
